a6afc9b7b89cba0d920458b5d0949e58417fb205
Simon Wendel <mail@simonwendel.se>
2022-06-03 15:05:34 +0200

    feat: add registration method to wire up storage

---
+++ backend/Storage/IoC.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Storage;
+
+public static class IoC
+{
+    public static IServiceCollection AddSecretsStorage(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddTransient<ITimestampGenerator, TimestampGenerator>();
+        serviceCollection.AddTransient<IBase36Converter, Base36Converter>();
+        serviceCollection.AddTransient<IIdGenerator, IdGenerator>();
+        serviceCollection.AddTransient<IRepository>(_ => MongoDbRepositoryFactory.Build());
+        return serviceCollection;
+    }
+}
