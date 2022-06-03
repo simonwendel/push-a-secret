57d5e9e6c81327fa691bc1ba29fecc37c0d9445b
Simon Wendel <mail@simonwendel.se>
2022-06-03 15:03:56 +0200

    feat: add factory building mongodb repo

---
+++ backend/Storage/MongoDbRepositoryFactory.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Storage;
+
+internal static class MongoDbRepositoryFactory
+{
+    private const string ConnectionString = "mongodb://localhost:27017";
+    private const string DatabaseName = "push-a-secret";
+    private const string CollectionName = "secrets";
+
+    public static MongoDbRepository Build()
+    {
+        var mongoClient = new MongoClient(ConnectionString);
+        var database = mongoClient.GetDatabase(DatabaseName);
+        var collection = database.GetCollection<BsonDocument>(CollectionName);
+        return new MongoDbRepository(collection);
+    }
+}
