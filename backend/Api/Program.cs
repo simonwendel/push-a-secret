2573c6b266f9dbb7007014a5e1046fe364269de2
Simon Wendel <mail@simonwendel.se>
2022-06-03 16:29:54 +0200

    feat: use lower case urls in routes

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -1,6 +1,7 @@
 using Storage;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
