e014110ddfbec567277c32691a718815d9626ee0
Simon Wendel <mail@simonwendel.se>
2022-06-16 20:40:03 +0200

    feat: make untrusted value equatable to wrapped type

---
+++ backend/Validation.Tests/UntrustedValueTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Xunit;
+
+namespace Validation.Tests;
+
+public class UntrustedValueTests
+{
+    private readonly SuperSecretTestClass value = new();
+    private readonly UntrustedValue<SuperSecretTestClass> untrusted;
+
+    public UntrustedValueTests()
+        => untrusted = new(value);
+
+    [Theory, AutoData]
+    public void Equals_Always_ReturnsEqualsForWrappedValue(object other)
+    {
+        value.Equals(other).Should().Be(value.EqualsResult);
+        value.EqualsCalled.Should().BeTrue();
+    }
+
+    [Fact]
+    public void GetHashCode_Always_ReturnsHashCodeForWrappedValue()
+    {
+        untrusted.GetHashCode().Should().Be(value.GetHashCodeResult);
+        value.GetHashCodeCalled.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ToString_Always_ThrowsExceptionInsteadOfReturningValue()
+    {
+        var rendering = () =>
+        {
+            var _ = untrusted.ToString();
+        };
+
+        rendering.Should().Throw<InvalidOperationException>();
+        value.ToStringCalled.Should().BeFalse();
+    }
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    private class SuperSecretTestClass
+    {
+        internal bool EqualsCalled { get; private set; }
+        internal bool EqualsResult { get; }
+
+        internal bool GetHashCodeCalled { get; private set; }
+        internal int GetHashCodeResult { get; }
+
+        internal bool ToStringCalled { get; private set; }
+        private string ToStringValue { get; }
+
+        public SuperSecretTestClass()
+        {
+            EqualsCalled = false;
+            EqualsResult = true;
+
+            GetHashCodeCalled = false;
+            GetHashCodeResult = 1337;
+
+            ToStringCalled = false;
+            ToStringValue = "1337";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            EqualsCalled = true;
+            return EqualsResult;
+        }
+
+        public override int GetHashCode()
+        {
+            // ReSharper disable once NonReadonlyMemberInGetHashCode
+            GetHashCodeCalled = true;
+            return GetHashCodeResult;
+        }
+
+        public override string ToString()
+        {
+            ToStringCalled = true;
+            return ToStringValue;
+        }
+    }
+}
--- backend/Validation/UntrustedValue.cs
+++ backend/Validation/UntrustedValue.cs
@@ -8,4 +8,26 @@ public class UntrustedValue<T>
     }
 
     internal T Value { get; }
+
+    public sealed override bool Equals(object? obj)
+        => Value!.Equals(obj);
+
+    public bool Equals(T other) 
+        => Equals((object?) other);
+
+    /// <remarks>
+    /// The GetHashCode() method can of course be used to reverse-engineer the underlying
+    /// value without validating it first, but there are easier ways than this if you really
+    /// want to, f.x. reflection.
+    /// </remarks>
+    public sealed override int GetHashCode() 
+        => Value!.GetHashCode();
+
+    /// <remarks>
+    /// In order to at least make it hard to bypass validation, ToString() will always throw an
+    /// exception and is sealed to prevent such silliness.
+    /// </remarks>>
+    /// <exception cref="InvalidOperationException">Always.</exception>
+    public sealed override string ToString()
+        => throw new InvalidOperationException("Bypassing validation by calling ToString() is not allowed.");
 }
