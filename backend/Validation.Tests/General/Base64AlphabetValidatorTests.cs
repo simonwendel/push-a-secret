d950423e6f50eb84bbb8645fa5109b779d35bee5
Simon Wendel <mail@simonwendel.se>
2022-06-17 22:43:02 +0200

    feat: validate base64 representations of BMP

---
+++ backend/Validation.Tests/General/Base64AlphabetValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Validation.General;
+using Xunit;
+
+namespace Validation.Tests.General;
+
+public class Base64AlphabetValidatorTests
+{
+    [Theory]
+    [InlineAutoData("-")]
+    [InlineAutoData("\\")]
+    [InlineAutoData("å")]
+    [InlineAutoData(";")]
+    public void Validate_GivenIllegalCharacter_ThrowsException(string untrusted, Base64AlphabetValidator sut)
+    {
+        Action validating = () => sut.Validate(new UntrustedValue<string>(untrusted));
+        validating.Should().Throw<ValidationException>();
+    }
+
+    [Theory]
+    [InlineAutoData("+/")]
+    [InlineAutoData("+/=")]
+    [InlineAutoData("+/===")]
+    [InlineAutoData("+/====")]
+    public void Validate_GivenInvalidPadding_ThrowsException(string value, Base64AlphabetValidator sut)
+    {
+        Action validating = () => sut.Validate(new UntrustedValue<string>(value));
+        validating.Should().Throw<ValidationException>();
+    }
+
+    [Theory]
+    [InlineAutoData("ABCDEFGHIJKLMNOPQRSTUVWXYZ==")]
+    [InlineAutoData("abcdefghijklmnopqrstuvwxyz==")]
+    [InlineAutoData("0123456789==")]
+    [InlineAutoData("+/==")]
+    [InlineAutoData(" ")]
+    [InlineAutoData("")]
+    public void Validate_GivenValidString_ReturnsValue(string value, Base64AlphabetValidator sut)
+    {
+        var untrusted = new UntrustedValue<string>(value);
+        sut.Validate(untrusted).Should().Be(value);
+    }
+}
---
+++ backend/Validation/General/Base64AlphabetValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace Validation.General;
+
+public class Base64AlphabetValidator : ValidatorBase<string>
+{
+    private protected override bool Valid(string value)
+        => Convert.TryFromBase64String(value, new byte[value.Length * 2], out _); // BMP is max 2 bytes per character
+}
