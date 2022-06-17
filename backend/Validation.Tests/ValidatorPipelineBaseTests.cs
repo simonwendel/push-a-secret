82fb87668a58547d8c7c642eec7f7675fecfa5fa
Simon Wendel <mail@simonwendel.se>
2022-06-17 17:44:52 +0200

    test: pipeline base class should allow empty pipeline

---
+++ backend/Validation.Tests/ValidatorPipelineBaseTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using Validation.General;
+using Xunit;
+
+namespace Validation.Tests;
+
+public class ValidatorPipelineBaseTests
+{
+    [Fact]
+    public void Ctor_GivenNoValidators_ThrowsException()
+    {
+        var constructing = () =>
+        {
+            var pipeline = new TestPipeline();
+        };
+
+        constructing.Should().Throw<ArgumentException>();
+    }
+
+    private class TestPipeline : ValidatorPipelineBase<object>
+    {
+        public TestPipeline()
+            : base(Array.Empty<ValidatorBase<object>>())
+        {
+        }
+    }
+}
