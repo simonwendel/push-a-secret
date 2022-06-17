6a7c508f344ad5a3511328a519d1ab485ffc060f
Simon Wendel <mail@simonwendel.se>
2022-06-17 17:48:48 +0200

    refactor: validation exception doesn't need to be serializable

--- backend/Validation/ValidationException.cs
+++ backend/Validation/ValidationException.cs
@@ -1,24 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿namespace Validation;
 
-namespace Validation;
-
-[Serializable]
 public class ValidationException : Exception
 {
-    public ValidationException()
-    {
-    }
-    
-    public ValidationException(string message) : base(message)
-    {
-    }
-    
-    public ValidationException(string message, Exception inner) : base(message, inner)
-    {
-    }
-    
-    protected ValidationException(SerializationInfo info, StreamingContext context)
-        : base(info, context)
-    {
-    }
 }
