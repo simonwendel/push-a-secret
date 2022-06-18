b31aa3f6a1d3be52aad1dfa0aa9bad98563dfa94
Simon Wendel <mail@simonwendel.se>
2022-06-18 22:43:08 +0200

    feat: add generic validator to check properties of objects

---
+++ backend/Validation.Tests/General/ObjectPropertyValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Validation.General;
+using Xunit;
+
+namespace Validation.Tests.General;
+
+public class ObjectPropertyValidatorTests
+{
+    [Fact]
+    public void Validate_GivenInvalidProperty_ThrowsException()
+    {
+        var (sut, obj) = CreateSystemFor(string.Empty);
+        Action validating = () => sut.Validate(new UntrustedValue<TestValue>(obj));
+        validating.Should().Throw<ValidationException>();
+    }
+
+    [Theory, AutoData]
+    public void Validate_GivenValidProperty_ReturnsValue(string value)
+    {
+        var (sut, obj) = CreateSystemFor(value);
+        sut.Validate(new UntrustedValue<TestValue>(obj)).TestProperty.Should().Be(value);
+    }
+
+    private class TestValue
+    {
+        public TestValue(string testProperty)
+        {
+            TestProperty = testProperty;
+        }
+
+        public string TestProperty { get; }
+    }
+
+    private (ObjectPropertyValidator<TestValue, string>, TestValue) CreateSystemFor(string value)
+    {
+        var validator = new NonEmptyStringValidator();
+        var obj = new TestValue(value);
+        var sut = new ObjectPropertyValidator<TestValue, string>(x => x.TestProperty, validator);
+        return (sut, obj);
+    }
+}
---
+++ backend/Validation/General/ObjectPropertyValidator.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq.Expressions;
+
+namespace Validation.General;
+
+public class ObjectPropertyValidator<T, TProp> : ValidatorBase<T>
+    where T : notnull
+    where TProp : notnull
+{
+    private readonly Expression<Func<T, TProp>> selector;
+    private readonly ValidatorBase<TProp> validator;
+
+    public ObjectPropertyValidator(Expression<Func<T, TProp>> selector, ValidatorBase<TProp> validator)
+    {
+        this.selector = selector;
+        this.validator = validator;
+    }
+
+    private protected override bool Valid(T value)
+    {
+        try
+        {
+            validator.Validate(new UntrustedValue<TProp>(selector.Compile().Invoke(value)));
+            return true;
+        }
+        catch (ValidationException)
+        {
+            return false;
+        }
+    }
+}
