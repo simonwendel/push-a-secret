7e93027d51b3c961972a9be3d6b88f26ae22a562
Simon Wendel <mail@simonwendel.se>
2022-06-18 15:54:29 +0200

    feat: validate algorithm name

---
+++ backend/Validation.Tests/AlgorithmValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Xunit;
+
+namespace Validation.Tests;
+
+public class AlgorithmValidatorTests
+{
+    [Fact]
+    public void Default_AsPerConfiguration_ShouldBeAesGcm128()
+        => AlgorithmValidator.DefaultAlgorithm.Should().Be("A128GCM");
+
+    [Theory]
+    [InlineAutoData("")]
+    [InlineAutoData(" ")]
+    [InlineAutoData("\n")]
+    [InlineAutoData("\t")]
+    [InlineAutoData("A256GCM")]
+    public void Validate_GivenOtherThanDefaultAlgorithm_ThrowsException(string algorithm, AlgorithmValidator sut)
+    {
+        Action validating = () => sut.Validate(new UntrustedValue<string>(algorithm));
+        validating.Should().Throw<ValidationException>();
+    }
+
+    [Theory, InlineAutoData("A128GCM")]
+    public void Validate_GivenDefaultAlgorithm_ReturnsValue(string algorithm, AlgorithmValidator sut)
+        => sut.Validate(new UntrustedValue<string>(algorithm)).Should().Be(algorithm);
+}
---
+++ backend/Validation/AlgorithmValidator.cs
@@ -0,0 +1,16 @@
+﻿using Validation.General;
+
+namespace Validation;
+
+public class AlgorithmValidator : ValidatorPipelineBase<string>
+{
+    public AlgorithmValidator()
+        : base(new NonEmptyStringValidator())
+    {
+    }
+
+    public const string DefaultAlgorithm = "A128GCM";
+
+    private protected override bool Valid(string value) 
+        => base.Valid(value) && value.Equals(DefaultAlgorithm);
+}
