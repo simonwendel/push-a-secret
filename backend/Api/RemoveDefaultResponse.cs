1a3d5476fbf88266d7a91b408bd977cc829d2d6c
Simon Wendel <mail@simonwendel.se>
2022-06-26 19:54:49 +0200

    docs: add a swashbuckle filter to remove all default 200's

---
+++ backend/Api/RemoveDefaultResponse.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Api;
+
+public class RemoveDefaultResponse : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (operation.Responses.TryGetValue("200", out var response)
+            && response.Description == "Success")
+        {
+            operation.Responses.Remove("200");
+        }
+    }
+}
