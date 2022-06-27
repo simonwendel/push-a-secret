af98e4978a5a347403feb7686d0c445292745ced
Simon Wendel <mail@simonwendel.se>
2022-06-27 10:10:37 +0200

    docs: annotate secret controller with responses and remarks

--- backend/Api/SecretController.cs
+++ backend/Api/SecretController.cs
@@ -1,10 +1,12 @@
-﻿using Domain;
+﻿using System.Net.Mime;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Validation;
 
 namespace Api;
 
-[ApiController, Route("[controller]")]
+[ApiController]
+[Route("[controller]")]
 public class SecretController : ControllerBase
 {
     private readonly IIdentifierValidator idValidator;
@@ -18,7 +20,22 @@ public class SecretController : ControllerBase
         this.secretValidator = secretValidator;
     }
 
+    /// <summary>
+    /// Check for secret in persistant storage.
+    /// </summary>
+    /// <remarks>
+    /// The response will never have a body. Issuing a HTTP GET request instead using the same
+    /// identifier will return the secret itself.
+    /// </remarks>
+    /// <param name="identifier">String identifier of the secret to look up.</param>
+    /// <returns>Empty response.</returns>
+    /// <response code="200">A secret matching supplied identifier can be retrieved from storage.</response>
+    /// <response code="400">Supplied identifier has incorrect format, or request is otherwise invalid.</response>
+    /// <response code="404">No secret in storage matches supplied identifier.</response>
     [HttpHead("{identifier}")]
+    [ProducesResponseType(200, Type = default!)]
+    [ProducesResponseType(400, Type = default!)]
+    [ProducesResponseType(404, Type = default!)]
     public IActionResult Head([FromRoute] UntrustedValue<string> identifier)
         => HandleRequestWithIdentifier<PeekRequest>(
             identifier,
@@ -28,7 +45,21 @@ public class SecretController : ControllerBase
                 _ => NotFound()
             });
 
+    /// <summary>
+    /// Retrieve a secret from persistant storage.
+    /// </summary>
+    /// <param name="identifier">String identifier of the secret to look up.</param>
+    /// <returns>Secret object, if successful, otherwise empty response.</returns>
+    /// <response code="200">A secret matching supplied identifier is retrieved from storage.</response>
+    /// <response code="400">Supplied identifier has incorrect format, or request is otherwise invalid.</response>
+    /// <response code="404">No secret in storage matches supplied identifier.</response>
+    /// <response code="409">A secret in storage matching supplied identifier does not pass validation.</response>
     [HttpGet("{identifier}")]
+    [ProducesResponseType(200, Type = typeof(Secret))]
+    [ProducesResponseType(400, Type = default!)]
+    [ProducesResponseType(404, Type = default!)]
+    [ProducesResponseType(409, Type = default!)]
+    [Produces(MediaTypeNames.Application.Json)]
     public IActionResult Get([FromRoute] UntrustedValue<string> identifier)
         => HandleRequestWithIdentifier<ReadRequest>(
             identifier,
@@ -42,16 +73,27 @@ public class SecretController : ControllerBase
                 _ => NotFound()
             });
 
+    /// <summary>
+    /// Save a secret to persistant storage.
+    /// </summary>
+    /// <param name="secret">A secret to persist to storage.</param>
+    /// <returns>Secret object, if successful, otherwise empty response.</returns>
+    /// <response code="201">A secret is persisted to storage. Location header contains resource URL.</response>
+    /// <response code="400">Supplied secret has incorrect format, or request is otherwise invalid.</response>
     [HttpPost]
+    [ProducesResponseType(201, Type = typeof(Secret))]
+    [ProducesResponseType(400, Type = default!)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
     public IActionResult Post([FromBody] UntrustedValue<Secret> secret)
     {
         try
         {
-            var s = secretValidator.Validate(secret);
-            return store.Create(new CreateRequest(s.Algorithm, s.IV, s.Ciphertext)) switch
+            var valid = secretValidator.Validate(secret);
+            return store.Create(new CreateRequest(valid.Algorithm, valid.IV, valid.Ciphertext)) switch
             {
-                (Result.OK, not null) response => Created(ConstructResourceUrl(response.Identifier!), s),
-                _ => StatusCode(500)
+                (Result.OK, not null) response => Created(ConstructResourceUrl(response.Identifier!), valid),
+                _ => StatusCode(500) // probably only happens if the db is down, we needn't add it to docs
             };
         }
         catch (ValidationException)
@@ -60,7 +102,18 @@ public class SecretController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Remove a secret from persistant storage.
+    /// </summary>
+    /// <param name="identifier">String identifier of the secret to delete.</param>
+    /// <returns>Empty response.</returns>
+    /// <response code="204">A secret matching supplied identifier is removed from storage.</response>
+    /// <response code="400">Supplied identifier has incorrect format, or request is otherwise invalid.</response>
+    /// <response code="404">No secret in storage matches supplied identifier.</response>
     [HttpDelete("{identifier}")]
+    [ProducesResponseType(204, Type = default!)]
+    [ProducesResponseType(400, Type = default!)]
+    [ProducesResponseType(404, Type = default!)]
     public IActionResult Delete([FromRoute] UntrustedValue<string> identifier)
         => HandleRequestWithIdentifier<DeleteRequest>(
             identifier,
