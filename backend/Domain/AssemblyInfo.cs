e6868b2657d06d457b797a00187760823edbdb87
Simon Wendel <mail@simonwendel.se>
2022-06-30 22:01:39 +0200

    chore: csproj tags exposing internals, not assembly attrs

--- backend/Domain/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Domain.Tests")]
-[assembly: InternalsVisibleTo("Verify.Benchmark")]
-[assembly: InternalsVisibleTo("Verify.Integration")]
