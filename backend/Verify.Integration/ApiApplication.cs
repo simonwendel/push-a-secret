61917906b5c6b54e5e9325067e8ecc2f39d4931a
Simon Wendel <mail@simonwendel.se>
2022-07-26 18:32:03 +0200

    test: clean up and reduce dupes in e2e test code

---
+++ backend/Verify.Integration/ApiApplication.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+using Domain;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Verify.Integration;
+
+internal class ApiApplication : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(x => x.ServiceType == typeof(IRepository));
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddSingleton<IRepository>(_ => TestMongoDbRepositoryFactory.Build(cleanAll: true));
+        });
+    }
+}
--- backend/Verify.Integration/EndToEndIntegrationTests.cs
+++ backend/Verify.Integration/EndToEndIntegrationTests.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -7,10 +6,6 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Domain;
 using FluentAssertions;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace Verify.Integration;
@@ -18,34 +13,34 @@ namespace Verify.Integration;
 [Collection("Verify.Integration")]
 public class EndToEndIntegrationTests
 {
-    private readonly Secret original;
+    private readonly Secret secret;
     private readonly HttpClient client;
 
     public EndToEndIntegrationTests()
     {
         var app = new ApiApplication();
         client = app.CreateClient();
-        original = new("A128GCM", "xMY6HokU51VT8g02", "zb8HP7LFqYr+1fWZA5ZFAfIHz3Y=");
+        secret = new("A128GCM", "xMY6HokU51VT8g02", "zb8HP7LFqYr+1fWZA5ZFAfIHz3Y=");
     }
 
     [Fact]
     internal async Task RoundTrip_WhenRunAgainstApi_WorksAsIntended()
         => await client
-            .InsertNewSecret(original)
+            .InsertNew(secret)
             .VerifyItExists()
             .VerifyItCanBeRead()
             .DeleteIt()
             .VerifyItIsGone();
 }
 
-internal static class EndToEndIntegrationTestExtensions
+internal static class FluentEndToEndIntegrationTestMethods
 {
     private static readonly JsonSerializerOptions options = new()
     {
         PropertyNameCaseInsensitive = true
     };
 
-    public static async Task<(HttpClient, Uri, Secret)> InsertNewSecret(
+    public static async Task<(HttpClient, Uri, Secret)> InsertNew(
         this HttpClient client, Secret original)
     {
         var response = await client.PostAsJsonAsync("secret", original);
@@ -61,60 +56,42 @@ internal static class EndToEndIntegrationTestExtensions
         throw new Xunit.Sdk.NotNullException();
     }
 
-    public static async Task<(HttpClient, Uri, Secret)> VerifyItExists(
-        this Task<(HttpClient, Uri, Secret)> previous)
-    {
-        var chain = await previous;
-        var (client, location, _) = chain;
-        var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, location));
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-        return chain;
-    }
-
-    public static async Task<(HttpClient, Uri, Secret)> VerifyItCanBeRead(
-        this Task<(HttpClient, Uri, Secret)> previous)
-    {
-        var chain = await previous;
-        var (client, location, original) = chain;
-        var response = await client.GetAsync(location);
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-        var secret = await response.Content.ReadFromJsonAsync<Secret>(options);
-        secret.Should().BeEquivalentTo(original);
-        return chain;
-    }
-
-    public static async Task<(HttpClient, Uri, Secret)> DeleteIt(
-        this Task<(HttpClient, Uri, Secret)> previous)
-    {
-        var chain = await previous;
-        var (client, location, _) = chain;
-        var response = await client.DeleteAsync(location);
-        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
-        return chain;
-    }
-
-    public static async Task VerifyItIsGone(this Task<(HttpClient, Uri, Secret)> previous)
-    {
-        var (client, location, _) = await previous;
-        var response = await client.GetAsync(location);
-        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
-    }
-}
-
-internal class ApiApplication : WebApplicationFactory<Program>
-{
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        base.ConfigureWebHost(builder);
-        builder.ConfigureTestServices(services =>
+    public static async Task<(HttpClient, Uri, Secret)> VerifyItExists(this Task<(HttpClient, Uri, Secret)> chain)
+        => await chain.Continue(async (client, location, _) =>
         {
-            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IRepository));
-            if (descriptor != null)
-            {
-                services.Remove(descriptor);
-            }
-
-            services.AddSingleton<IRepository>(_ => TestMongoDbRepositoryFactory.Build(cleanAll: true));
+            var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, location));
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
         });
+
+    public static async Task<(HttpClient, Uri, Secret)> VerifyItCanBeRead(this Task<(HttpClient, Uri, Secret)> chain)
+        => await chain.Continue(async (client, location, original) =>
+        {
+            var response = await client.GetAsync(location);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var secret = await response.Content.ReadFromJsonAsync<Secret>(options);
+            secret.Should().BeEquivalentTo(original);
+        });
+
+    public static async Task<(HttpClient, Uri, Secret)> DeleteIt(this Task<(HttpClient, Uri, Secret)> chain)
+        => await chain.Continue(async (client, location, _) =>
+        {
+            var response = await client.DeleteAsync(location);
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        });
+
+    public static async Task VerifyItIsGone(this Task<(HttpClient, Uri, Secret)> chain)
+        => await chain.Continue(async (client, location, _) =>
+        {
+            var response = await client.GetAsync(location);
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        });
+
+    private static async Task<(HttpClient, Uri, Secret)> Continue(
+        this Task<(HttpClient, Uri, Secret)> previous, Func<HttpClient, Uri, Secret, Task> next)
+    {
+        var chain = await previous;
+        var (client, location, secret) = chain;
+        await next(client, location, secret);
+        return chain;
     }
 }
