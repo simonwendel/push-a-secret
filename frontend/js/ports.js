75a16a6b937a56377f330c23eb52fe09bfe4b5f0
Simon Wendel <mail@simonwendel.se>
2022-07-31 10:25:49 +0200

    refactor: remove old localstorage ports

--- frontend/js/ports.js
+++ frontend/js/ports.js
@@ -1,6 +1,5 @@
-export { wireCrypto, wireStorage }
+export { wireCrypto }
 
-import { check, retrieve, store, remove } from './storage.js';
 import { generate, encrypt, decrypt } from '/crypto.js'
 
 const wireCrypto = app => {
@@ -24,38 +23,6 @@ const wireCrypto = app => {
         const decrypted = await decrypt(request);
         app.ports.receiveDecryption.send({
             cleartext: decrypted.cleartext
-        });
-    });
-}
-
-const wireStorage = app => {
-    app.ports.requestPeek.subscribe(request => {
-        const exists = check(request.id);
-        app.ports.receivePeek.send({
-            exists: exists
-        });
-    });
-
-    app.ports.requestCreate.subscribe(request => {
-        const stored = store(request);
-        app.ports.receiveCreate.send({
-            id: stored.id
-        });
-    });
-
-    app.ports.requestRead.subscribe(request => {
-        const stored = retrieve(request.id);
-        app.ports.receiveRead.send({
-            algorithm: stored.algorithm,
-            iv: stored.iv,
-            ciphertext: stored.ciphertext
-        });
-    });
-    
-    app.ports.requestDelete.subscribe(request => {
-        remove(request.id);
-        app.ports.receiveDelete.send({
-            success: true
         });
     });
 }
