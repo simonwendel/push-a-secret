e717ea9dd80e2d0da82ca784c97980d9ad2611bf
Simon Wendel <mail@simonwendel.se>
2022-07-31 10:22:43 +0200

    feat: add an api client to use backend

---
+++ frontend/src/Api.elm
@@ -0,0 +1,134 @@
+module Api exposing (Client, Headers, client)
+
+import Dict exposing (Dict)
+import Http exposing (Expect)
+import Json.Decode as D exposing (Decoder)
+import Json.Encode as E
+
+
+type alias Headers =
+    Dict String String
+
+
+type alias RequestExpectEmpty msg =
+    String -> (Result Http.Error ( (), Headers ) -> msg) -> Cmd msg
+
+
+type alias RequestExpectJson msg a =
+    String -> Decoder a -> (Result Http.Error ( a, Headers ) -> msg) -> Cmd msg
+
+
+type alias RequestWithJsonExpectJson msg a =
+    E.Value -> Decoder a -> (Result Http.Error ( a, Headers ) -> msg) -> Cmd msg
+
+
+type alias Client msg a =
+    { head : RequestExpectEmpty msg
+    , get : RequestExpectJson msg a
+    , post : RequestWithJsonExpectJson msg a
+    , delete : RequestExpectEmpty msg
+    }
+
+
+client : Client msg a
+client =
+    buildClient "http://localhost:5000"
+
+
+buildClient : String -> Client msg a
+buildClient baseUrl =
+    let
+        makeUrl =
+            (++) <| baseUrl ++ "/secret/"
+    in
+    { head = \id msg -> headRequest (makeUrl id) msg
+    , get = \id decoder msg -> getRequest (makeUrl id) decoder msg
+    , post = \secret decoder msg -> postRequest (makeUrl "") secret decoder msg
+    , delete = \id msg -> deleteRequest (makeUrl id) msg
+    }
+
+
+headRequest : RequestExpectEmpty msg
+headRequest url msg =
+    makeRequest
+        { url = url
+        , method = "HEAD"
+        , body = Http.emptyBody
+        , expect = responseWithMetadata msg ignoreBody
+        }
+
+
+getRequest : RequestExpectJson msg a
+getRequest url decoder msg =
+    Http.get
+        { url = url
+        , expect = responseWithMetadata msg (parseJsonBody decoder)
+        }
+
+
+postRequest : String -> RequestWithJsonExpectJson msg a
+postRequest url value decoder msg =
+    Http.post
+        { url = url
+        , body = Http.jsonBody value
+        , expect = responseWithMetadata msg (parseJsonBody decoder)
+        }
+
+
+deleteRequest : RequestExpectEmpty msg
+deleteRequest url msg =
+    makeRequest
+        { url = url
+        , method = "DELETE"
+        , body = Http.emptyBody
+        , expect = responseWithMetadata msg ignoreBody
+        }
+
+
+makeRequest : { url : String, expect : Expect msg, method : String, body : Http.Body } -> Cmd msg
+makeRequest params =
+    Http.request
+        { url = params.url
+        , expect = params.expect
+        , method = params.method
+        , body = params.body
+        , headers = []
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+ignoreBody : Http.Metadata -> String -> Result error ( (), Headers )
+ignoreBody metadata _ =
+    Ok ( (), metadata.headers )
+
+
+parseJsonBody : Decoder a -> Http.Metadata -> String -> Result Http.Error ( a, Headers )
+parseJsonBody decoder metadata body =
+    case D.decodeString decoder body of
+        Ok decoded ->
+            Ok ( decoded, metadata.headers )
+
+        Err err ->
+            Err (Http.BadBody (D.errorToString err))
+
+
+responseWithMetadata : (Result Http.Error ( a, Headers ) -> msg) -> (Http.Metadata -> String -> Result Http.Error ( a, Headers )) -> Expect msg
+responseWithMetadata msg parse =
+    Http.expectStringResponse msg <|
+        \response ->
+            case response of
+                Http.BadUrl_ url ->
+                    Err (Http.BadUrl url)
+
+                Http.Timeout_ ->
+                    Err Http.Timeout
+
+                Http.NetworkError_ ->
+                    Err Http.NetworkError
+
+                Http.BadStatus_ metadata _ ->
+                    Err (Http.BadStatus metadata.statusCode)
+
+                Http.GoodStatus_ metadata body ->
+                    parse metadata body
