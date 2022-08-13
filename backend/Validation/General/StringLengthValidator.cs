81bb3f514527843e8043074bf2dea961230cac0f
Simon Wendel <mail@simonwendel.se>
2022-08-13 20:09:40 +0200

    refactor: use interval validator for string length validation

--- backend/Validation/General/StringLengthValidator.cs
+++ backend/Validation/General/StringLengthValidator.cs
@@ -1,33 +1,12 @@
 ﻿namespace Validation.General;
 
-internal class StringLengthValidator : ValidatorBase<string>
+internal class StringLengthValidator : ValidatorPipelineBase<string>
 {
-    private readonly int minLength;
-    private readonly int maxLength;
-
     public StringLengthValidator(int minLength, int maxLength = int.MaxValue)
+        : base(
+            new ObjectPropertyValidator<string, int>(
+                x => x.Length,
+                new IntegerIntervalValidator(minLength, maxLength)))
     {
-        if (minLength < 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(minLength));
-        }
-
-        if (maxLength < 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(maxLength));
-        }
-
-        if (maxLength < minLength)
-        {
-            throw new ArgumentException();
-        }
-
-        this.minLength = minLength;
-        this.maxLength = maxLength;
-    }
-
-    private protected override bool Valid(string value)
-    {
-        return value.Length >= minLength && value.Length <= maxLength;
     }
 }
