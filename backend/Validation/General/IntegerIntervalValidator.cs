41401c35f0d4185d938c4f33cabbf76de75c0e1a
Simon Wendel <mail@simonwendel.se>
2022-08-13 20:08:54 +0200

    feat: add validator checking if integer is within interval

---
+++ backend/Validation/General/IntegerIntervalValidator.cs
@@ -0,0 +1,33 @@
+﻿namespace Validation.General;
+
+internal class IntegerIntervalValidator : ValidatorBase<int>
+{
+    private readonly int lowerBounds;
+    private readonly int upperBounds;
+
+    public IntegerIntervalValidator(int lowerBounds, int upperBounds = int.MaxValue)
+    {
+        if (lowerBounds < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(lowerBounds));
+        }
+
+        if (upperBounds < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(upperBounds));
+        }
+
+        if (upperBounds < lowerBounds)
+        {
+            throw new ArgumentException();
+        }
+
+        this.lowerBounds = lowerBounds;
+        this.upperBounds = upperBounds;
+    }
+
+    private protected override bool Valid(int value)
+    {
+        return value >= lowerBounds && value <= upperBounds;
+    }
+}
