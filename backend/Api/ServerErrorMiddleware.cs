2db16a07a60b76c6a84deb72e2fa1be87da1160b
Simon Wendel <mail@simonwendel.se>
2022-08-21 17:29:49 +0200

    refactor: rename field, removing _

--- backend/Api/ServerErrorMiddleware.cs
+++ backend/Api/ServerErrorMiddleware.cs
@@ -17,16 +17,16 @@ namespace Api;
 /// </remarks>
 public class ServerErrorMiddleware
 {
-    private readonly RequestDelegate _next;
+    private readonly RequestDelegate next;
 
     public ServerErrorMiddleware(RequestDelegate next) 
-        => _next = next;
+        => this.next = next;
 
     public async Task InvokeAsync(HttpContext context)
     {
         try
         {
-            await _next(context);
+            await next(context);
         }
         catch (Exception)
         {
