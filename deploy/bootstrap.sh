57168e5b0963b9f343b62be1fb358c8664fefbf7
Simon Wendel <mail@simonwendel.se>
2024-07-13 15:50:17 +0000

    fix: remove refs to chain.pem entirely

--- deploy/bootstrap.sh
+++ deploy/bootstrap.sh
@@ -95,7 +95,6 @@ SCRIPT=$(cat << END
         -keyout /etc/letsencrypt/live/\$d/privkey.pem \
         -out /etc/letsencrypt/live/\$d/fullchain.pem \
         -subj /CN=localhost
-      cp /etc/letsencrypt/live/\$d/fullchain.pem /etc/letsencrypt/live/\$d/chain.pem
     done
 END
 )
